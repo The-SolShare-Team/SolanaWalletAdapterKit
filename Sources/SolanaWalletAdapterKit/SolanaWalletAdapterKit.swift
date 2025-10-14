fcb842ef0f1b4778b4017171267bc10512e74960
Dan Chiem <dan@chiem.me>
2025-10-13 22:21:05 -0400

    Improve printing of HTTP request

--- Sources/SolanaWalletAdapterKit/SolanaWalletAdapterKit.swift
+++ Sources/SolanaWalletAdapterKit/SolanaWalletAdapterKit.swift
@@ -15,17 +15,28 @@ public func hello() -> String {
 
 class RpcNetworkDriver: RpccoreHttpNetworkDriver {
     func makeHttpRequest(request: RpccoreHttpRequest, completionHandler: @escaping @Sendable (String?, (any Error)?) -> Void) {
-        print(request.body, request.method, request.properties, request.url)
+        print("**********************************")
+        print("\nBody: ", request.body)
+        print("\nMethod: ", request.method)
+        print("\nProperties: ", request.properties)
+        print("\nURL: ", request.url)
+        print("**********************************")
     }
 }
 
 public func test() async {
+    // Setup RPC driver
     let rpcUrl = "https://api.endpoint.com"
     let rpcDriver = RpccoreRpc20Driver(url: rpcUrl, httpDriver: RpcNetworkDriver())
     
+    // Build RPC request
     let requestId = UUID().uuidString
     let requestMethod = "getTheThing"
-    
     let rpcRequest = RpccoreJsonRpc20Request(method: requestMethod, params: nil, id: requestId)
-    try? await rpcDriver.makeRequest(request: rpcRequest, resultSerializer: Kotlinx_serialization_jsonJsonElement.companion.serializer())
+    
+    // Send the request and get response
+    try? await rpcDriver.makeRequest(
+        request: rpcRequest,
+        resultSerializer: Kotlinx_serialization_jsonJsonElement.companion.serializer()
+    )
 }
