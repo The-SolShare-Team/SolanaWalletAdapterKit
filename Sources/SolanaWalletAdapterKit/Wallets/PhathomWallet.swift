a49bde584f60a3fc14eed5911c64ccc63532fbad
Dan Chiem <dan@chiem.me>
2025-10-14 20:18:13 -0400

    Move wallets to seperate folder

---
+++ Sources/SolanaWalletAdapterKit/Wallets/PhathomWallet.swift
@@ -0,0 +1,27 @@
+struct PhantomWallet: Wallet {
+    var dappEncryptionPublicKey: String
+    
+    func connect(appUrl: String, redirectLink: String, cluster: String?) async throws {
+        // Implementation
+    }
+    func disconnect(nonce: String, redirectLink: String, payload: String) async throws{
+        // Implementation
+    }
+    
+    func signAndSendTransaction(nonce: String, redirectLink: String, payload: String) async throws {
+        // Implementation
+    }
+    func signAllTransactions(nonce: String, redirectLink: String, payload: String) async throws {
+        // Implementation
+    }
+    func signTransaction(nonce: String, redirectLink: String, payload: String) async throws {
+        // Implementation
+    }
+    func signMessage(nonce: String, redirectLink: String, payload: String) async throws {
+        // Implementation
+    }
+    
+    func browse(url: String, ref: String) async throws {
+        // Implementation
+    }
+}
