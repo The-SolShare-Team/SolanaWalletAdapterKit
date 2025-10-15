22ce652bad30b7b81110251cc45daa8f9836b004
Dan Chiem <dan@chiem.me>
2025-10-15 01:48:07 -0400

    Move network driver to a seperate file

--- Sources/SolanaWalletAdapterKit/SolanaWalletAdapterKit.swift
+++ Sources/SolanaWalletAdapterKit/SolanaWalletAdapterKit.swift
@@ -1,6 +1,3 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
-
 import SolanaKit
 import CryptoKit
 
@@ -12,21 +9,10 @@ public func hello() -> String {
     return solanaKey.address
 }
 
-class RpcNetworkDriver: RpccoreHttpNetworkDriver {
-    func makeHttpRequest(request: RpccoreHttpRequest, completionHandler: @escaping @Sendable (String?, (any Error)?) -> Void) {
-        print("**********************************")
-        print("\nBody: ", request.body)
-        print("\nMethod: ", request.method)
-        print("\nProperties: ", request.properties)
-        print("\nURL: ", request.url)
-        print("**********************************")
-    }
-}
-
 public func test() async {
     // Setup RPC driver
     let rpcUrl = "https://api.endpoint.com"
-    let rpcDriver = RpccoreRpc20Driver(url: rpcUrl, httpDriver: RpcNetworkDriver())
+    let rpcDriver = RpccoreRpc20Driver(url: rpcUrl, httpDriver: NetworkDriver())
     
     // Build RPC request
     let requestId = UUID().uuidString
@@ -34,7 +20,7 @@ public func test() async {
     let rpcRequest = RpccoreJsonRpc20Request(method: requestMethod, params: nil, id: requestId)
     
     // Send the request and get response
-    try? await rpcDriver.makeRequest(
+    let rpcResponse = try? await rpcDriver.makeRequest(
         request: rpcRequest,
         resultSerializer: Kotlinx_serialization_jsonJsonElement.companion.serializer()
     )
