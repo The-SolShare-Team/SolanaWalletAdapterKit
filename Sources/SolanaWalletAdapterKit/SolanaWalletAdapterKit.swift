e57f45ccc3ac3946e203b9334fb5152b6dc9984b
Dan Chiem <dan@chiem.me>
2025-10-26 01:39:45 -0400

    Update dependencies (#4)

--- Sources/SolanaWalletAdapterKit/SolanaWalletAdapterKit.swift
+++ Sources/SolanaWalletAdapterKit/SolanaWalletAdapterKit.swift
@@ -1,27 +1,27 @@
-import SolanaKit
-import CryptoKit
-
-public func hello() -> String {
-    let privateKey = Curve25519.Signing.PrivateKey()
-    let publicKey = privateKey.publicKey
-    
-    let solanaKey = SolanaPublicKey(bytes: ByteArrayKt.toByteArray(publicKey.rawRepresentation))
-    return solanaKey.address
-}
-
-public func test() async {
-    // Setup RPC driver
-    let rpcUrl = "https://api.endpoint.com"
-    let rpcDriver = RpccoreRpc20Driver(url: rpcUrl, httpDriver: NetworkDriver())
-    
-    // Build RPC request
-    let requestId = UUID().uuidString
-    let requestMethod = "getTheThing"
-    let rpcRequest = RpccoreJsonRpc20Request(method: requestMethod, params: nil, id: requestId)
-    
-    // Send the request and get response
-    let rpcResponse = try? await rpcDriver.makeRequest(
-        request: rpcRequest,
-        resultSerializer: Kotlinx_serialization_jsonJsonElement.companion.serializer()
-    )
-}
+//import SolanaKit
+//import CryptoKit
+//
+//public func hello() -> String {
+//    let privateKey = Curve25519.Signing.PrivateKey()
+//    let publicKey = privateKey.publicKey
+//    
+//    let solanaKey = SolanaPublicKey(bytes: ByteArrayKt.toByteArray(publicKey.rawRepresentation))
+//    return solanaKey.address
+//}
+//
+//public func test() async {
+//    // Setup RPC driver
+//    let rpcUrl = "https://api.endpoint.com"
+//    let rpcDriver = RpccoreRpc20Driver(url: rpcUrl, httpDriver: NetworkDriver())
+//    
+//    // Build RPC request
+//    let requestId = UUID().uuidString
+//    let requestMethod = "getTheThing"
+//    let rpcRequest = RpccoreJsonRpc20Request(method: requestMethod, params: nil, id: requestId)
+//    
+//    // Send the request and get response
+//    let rpcResponse = try? await rpcDriver.makeRequest(
+//        request: rpcRequest,
+//        resultSerializer: Kotlinx_serialization_jsonJsonElement.companion.serializer()
+//    )
+//}
