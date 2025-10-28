8ef44489f9e2de34cc12105297263bd4bdbf6739
Samuel Martineau <samumartineau@gmail.com>
2025-10-28 01:09:53 -0400

    implement deeplinkfetcher

--- Sources/SolanaWalletAdapterKit/Wallets/SolflareWallet.swift
+++ Sources/SolanaWalletAdapterKit/Wallets/SolflareWallet.swift
@@ -1,27 +1,10 @@
-struct SolflareWallet: Wallet {
-    var dappEncryptionPublicKey: String
-    
-    func connect(appUrl: String, redirectLink: String, cluster: String?) async throws {
-        // Implementation
-    }
-    func disconnect(nonce: String, redirectLink: String, payload: String) async throws{
-        // Implementation
-    }
-    
+import Foundation
+
+struct SolflareWallet: DeeplinkWallet {
+    let baseURL = URL(string: "https://solflare.com/ul/v1")!
+    var dappEncryptionPublicKey = "test"
+
     func signAndSendTransaction(nonce: String, redirectLink: String, payload: String) async throws {
-        // Implementation
-    }
-    func signAllTransactions(nonce: String, redirectLink: String, payload: String) async throws {
-        // Implementation
-    }
-    func signTransaction(nonce: String, redirectLink: String, payload: String) async throws {
-        // Implementation
-    }
-    func signMessage(nonce: String, redirectLink: String, payload: String) async throws {
-        // Implementation
-    }
-    
-    func browse(url: String, ref: String) async throws {
         // Implementation
     }
 }
