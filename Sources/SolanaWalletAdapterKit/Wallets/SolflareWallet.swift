c4aca78633b63b49fc9aa2bc4001aa7bb832b4f1
Samuel Martineau <samumartineau@gmail.com>
2025-11-02 20:12:34 -0500

    Revert "Scaffold Wallets (#8)" (#18)
    This reverts commit b49ec1acd5d38a39b51a0c5c284fcf59bf1daf5b.

--- Sources/SolanaWalletAdapterKit/Wallets/SolflareWallet.swift
+++ Sources/SolanaWalletAdapterKit/Wallets/SolflareWallet.swift
@@ -1,10 +1,27 @@
-import Foundation
-
-struct SolflareWallet: DeeplinkWallet {
-    let baseURL = URL(string: "https://solflare.com/ul/v1")!
-    var dappEncryptionPublicKey = "test"
-
+struct SolflareWallet: Wallet {
+    var dappEncryptionPublicKey: String
+    
+    func connect(appUrl: String, redirectLink: String, cluster: String?) async throws {
+        // Implementation
+    }
+    func disconnect(nonce: String, redirectLink: String, payload: String) async throws{
+        // Implementation
+    }
+    
     func signAndSendTransaction(nonce: String, redirectLink: String, payload: String) async throws {
+        // Implementation
+    }
+    func signAllTransactions(nonce: String, redirectLink: String, payload: String) async throws {
+        // Implementation
+    }
+    func signTransaction(nonce: String, redirectLink: String, payload: String) async throws {
+        // Implementation
+    }
+    func signMessage(nonce: String, redirectLink: String, payload: String) async throws {
+        // Implementation
+    }
+    
+    func browse(url: String, ref: String) async throws {
         // Implementation
     }
 }
