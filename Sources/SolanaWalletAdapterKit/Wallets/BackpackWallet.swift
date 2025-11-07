0a9997f3960473fac531c2a6a1f873fa158a3f8a
Dan Chiem <dan@chiem.me>
2025-11-06 22:08:43 -0500

    Make wallets conform to DeeplinkWallet

--- Sources/SolanaWalletAdapterKit/Wallets/BackpackWallet.swift
+++ Sources/SolanaWalletAdapterKit/Wallets/BackpackWallet.swift
@@ -1,27 +1,5 @@
-struct BackpackWallet: Wallet {
-    var dappEncryptionPublicKey: String
-    
-    func connect(appUrl: String, redirectLink: String, cluster: String?) async throws {
-        // Implementation
-    }
-    func disconnect(nonce: String, redirectLink: String, payload: String) async throws{
-        // Implementation
-    }
-    
-    func signAndSendTransaction(nonce: String, redirectLink: String, payload: String) async throws {
-        // Implementation
-    }
-    func signAllTransactions(nonce: String, redirectLink: String, payload: String) async throws {
-        // Implementation
-    }
-    func signTransaction(nonce: String, redirectLink: String, payload: String) async throws {
-        // Implementation
-    }
-    func signMessage(nonce: String, redirectLink: String, payload: String) async throws {
-        // Implementation
-    }
-    
-    func browse(url: String, ref: String) async throws {
-        // Implementation
-    }
+import Foundation
+
+public struct BackpackWallet: DeeplinkWallet {
+    public static let baseURL = URL(string: "https://backpack.app/ul/v1")!
 }
--- Sources/SolanaWalletAdapterKit/Wallets/PhathomWallet.swift
+++ Sources/SolanaWalletAdapterKit/Wallets/PhathomWallet.swift
@@ -1,27 +1,5 @@
-struct PhantomWallet: Wallet {
-    var dappEncryptionPublicKey: String
-    
-    func connect(appUrl: String, redirectLink: String, cluster: String?) async throws {
-        // Implementation
-    }
-    func disconnect(nonce: String, redirectLink: String, payload: String) async throws{
-        // Implementation
-    }
-    
-    func signAndSendTransaction(nonce: String, redirectLink: String, payload: String) async throws {
-        // Implementation
-    }
-    func signAllTransactions(nonce: String, redirectLink: String, payload: String) async throws {
-        // Implementation
-    }
-    func signTransaction(nonce: String, redirectLink: String, payload: String) async throws {
-        // Implementation
-    }
-    func signMessage(nonce: String, redirectLink: String, payload: String) async throws {
-        // Implementation
-    }
-    
-    func browse(url: String, ref: String) async throws {
-        // Implementation
-    }
+import Foundation
+
+public struct PhantomWallet: DeeplinkWallet {
+    public static let baseURL = URL(string: "https://phantom.app/ul/v1")!
 }
