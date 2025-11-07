10ebd807cd51af703f154a17067eac5da5aa1f10
Dan Chiem <dan@chiem.me>
2025-11-06 23:57:24 -0500

    Using structs

--- Sources/SolanaWalletAdapterKit/SolanaWalletAdapterKit.swift
+++ Sources/SolanaWalletAdapterKit/SolanaWalletAdapterKit.swift
@@ -1,14 +1,29 @@
 import Foundation
 
-public class SolanaWalletAdapter {
-    public var fetcher: DeeplinkFetcher
-
-    public init(callbackScheme: String) {
-        fetcher = DeeplinkFetcher(scheme: callbackScheme)
+@MainActor
+public enum SolanaWalletAdapter {
+    private static var _fetcher: DeeplinkFetcher?
+    private static var fetcher: DeeplinkFetcher {
+        guard let fetcher = _fetcher else {
+            fatalError(
+                "SolanaWalletAdapter not initialized. Call registerCallbackScheme(_:) first.")
+        }
+        return fetcher
     }
 
-    @MainActor
-    public func handleOnOpenURL(_ url: URL) -> Bool {
+    public static func registerCallbackScheme(_ scheme: String) {
+        _fetcher = DeeplinkFetcher(scheme: scheme)
+    }
+
+    public static func handleOnOpenURL(_ url: URL) -> Bool {
         return fetcher.handleCallback(url)
+    }
+
+    public static func deeplinkFetch(
+        _ url: URL, callbackParameter: String, timeout: TimeInterval = 30.0
+    )
+        async throws(DeeplinkFetchingError) -> URLComponents
+    {
+        try await fetcher.fetch(url, callbackParameter: callbackParameter, timeout: timeout)
     }
 }
