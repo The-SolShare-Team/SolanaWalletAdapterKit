30125686c8ffcdcb1501a5717e78d813c0995324
Dan Chiem <dan@chiem.me>
2025-11-09 05:48:37 -0500

    Add wallet methods to DeeplinkWallet

--- Sources/SolanaWalletAdapterKit/Wallet.swift
+++ Sources/SolanaWalletAdapterKit/Wallet.swift
@@ -16,27 +16,123 @@ public protocol Wallet {
 =======
 import Foundation
 import SolanaRPC
+import SolanaTransactions
 
-public nonisolated protocol Wallet {
-    init(for appId: AppIdentity, cluster: Endpoint)
+@MainActor
+public protocol Wallet {
+    init(for: AppIdentity, cluster: Endpoint, restoreFrom: SecureStorage) async throws
 
-    mutating func connect(appUrl: String, redirectLink: String, cluster: String?)
-        async throws
-    mutating func disconnect(nonce: String, redirectLink: String, payload: String)
-        async throws
+    var appId: AppIdentity { get set }
+    var cluster: Endpoint { get set }
+    var secureStorage: SecureStorage { get set }
+    var connection: WalletConnection? { get set }
 
-    nonmutating func signAndSendTransaction(
-        nonce: String, redirectLink: String, payload: String)
+    mutating func pair()
         async throws
-    nonmutating func signAllTransactions(
-        nonce: String, redirectLink: String, payload: String)
-        async throws
-    nonmutating func signTransaction(
-        nonce: String, redirectLink: String, payload: String)
-        async throws
-    nonmutating func signMessage(nonce: String, redirectLink: String, payload: String)
+    mutating func unpair(nonce: String, redirectLink: String, payload: String)
         async throws
 
-    nonmutating func browse(url: URL, ref: String) async throws
->>>>>>> 4d7a344 (Using structs):Sources/SolanaWalletAdapterKit/Wallet.swift
+    nonmutating func signAndSendTransaction(transaction: Transaction, sendOptions: SendOptions?)
+        async throws -> SignAndSendTransactionResponse
+    nonmutating func signAllTransactions(transactions: [Transaction])
+        async throws -> SignAllTransactionsResponse
+    nonmutating func signTransaction(transaction: Transaction)
+        async throws -> SignTransactionResponse
+    nonmutating func signMessage(message: Data, display: DisplayFormat?)
+        async throws -> SignMessageResponse
+
+    nonmutating func browse(url: URL, ref: URL) async throws
+}
+
+public struct WalletConnection: Codable {
+    let encryption: DiffieHellmanData
+    let walletPublicKey: String
+    let session: String
+
+    public init(encryption: DiffieHellmanData, walletPublicKey: String, session: String) {
+        self.encryption = encryption
+        self.walletPublicKey = walletPublicKey
+        self.session = session
+    }
+}
+
+public struct DiffieHellmanData: Codable {
+    let publicKey: Data
+    let privateKey: Data
+    let sharedKey: Data
+
+    public init(publicKey: Data, privateKey: Data, sharedKey: Data) {
+        self.publicKey = publicKey
+        self.privateKey = privateKey
+        self.sharedKey = sharedKey
+    }
+}
+
+public struct SignAndSendTransactionResponse: Decodable {
+    public let signature: String
+}
+
+public struct SignAllTransactionsResponse: Decodable {
+    public let transactions: [String]
+}
+
+public struct SignTransactionResponse: Decodable {
+    public let transaction: String
+}
+
+public struct SignMessageResponse: Decodable {
+    public let signature: String
+}
+
+// SendOptions type based on https://solana-foundation.github.io/solana-web3.js/types/SendOptions.html
+public struct SendOptions: Codable {
+    public let maxRetries: Int?
+    public let minContextSlot: Int?
+    public let preflightCommitment: Commitment?
+    public let skipPreflight: Bool?
+}
+
+public enum DisplayFormat: String {
+    case hex = "hex"
+    case utf8 = "utf-8"
+}
+
+public enum WalletAdapterError: Error {
+    case notConnected
+    case invalidResponse
+    case pairingFailed
+    case unpairingFailed
+    case transactionSigningFailed
+    case messageSigningFailed
+    case browsingFailed
+}
+
+/// Error from the wallets:
+/// - Solflare: https://docs.solflare.com/solflare/technical/deeplinks/limitations#errors
+/// - Backpack: https://docs.backpack.app/deeplinks/limitations#errors
+/// - Phantom: https://docs.phantom.com/solana/errors
+enum WalletError: Error, LocalizedError {
+    case disconnected(message: String)
+    case unauthorized(message: String)
+    case userRejectedRequest(message: String)
+    case invalidInput(message: String)
+    case resourceNotAvailable(message: String)
+    case transactionRejected(message: String)
+    case methodNotFound(message: String)
+    case internalError(message: String)
+    case unknownError(code: Int, message: String)
+
+    init(code: Int, message: String) {
+        switch code {
+        case 4900: self = .disconnected(message: message)
+        case 4100: self = .unauthorized(message: message)
+        case 4001: self = .userRejectedRequest(message: message)
+        case -32000: self = .invalidInput(message: message)
+        case -32002: self = .resourceNotAvailable(message: message)
+        case -32003: self = .transactionRejected(message: message)
+        case -32601: self = .methodNotFound(message: message)
+        case -32603: self = .internalError(message: message)
+        default: self = .unknownError(code: code, message: message)
+        }
+    }
 }
--- Sources/SolanaWalletAdapterKit/Wallets/SolflareWallet.swift
+++ Sources/SolanaWalletAdapterKit/Wallets/SolflareWallet.swift
@@ -1,6 +1,22 @@
 import Foundation
+import SolanaRPC
 
-public struct SolflareWallet: DeeplinkWallet {
-    public static let baseURL = URL(string: "https://solflare.com/ul/v1")!
-    public var encryption: DiffieHellman
+public class SolflareWallet: DeeplinkWallet {
+    public static var baseURL: URL = URL(string: "https://solflare.com/ul/v1")!
+    public var connection: WalletConnection?
+    public var appId: AppIdentity
+    public var cluster: SolanaRPC.Endpoint
+    public var secureStorage: SecureStorage
+
+    public required init(
+        for appId: AppIdentity,
+        cluster: SolanaRPC.Endpoint,
+        restoreFrom secureStorage: SecureStorage
+    ) async throws {
+        self.appId = appId
+        self.cluster = cluster
+        self.secureStorage = secureStorage
+        self.connection = try await secureStorage.retrieveWalletConnection(
+            key: self.secureStorageKey)
+    }
 }
