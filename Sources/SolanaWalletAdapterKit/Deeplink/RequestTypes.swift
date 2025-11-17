df051e83ccd1c82f891d4f788028317a447dc92f
Dan Chiem <dan@chiem.me>
2025-11-17 01:37:59 -0500

    Refactor DeeplinkWallet

--- Sources/SolanaWalletAdapterKit/Deeplink/RequestTypes.swift
+++ Sources/SolanaWalletAdapterKit/Deeplink/RequestTypes.swift
@@ -1,4 +1,51 @@
+import Foundation
 import SolanaRPC
+
+// ***********************************
+// Method Types
+// ***********************************
+
+struct DisconnectRequestPayload: Encodable, Sendable {
+    let session: String
+}
+
+struct SignAndSendTransactionRequestPayload: Encodable, Sendable {
+    let transaction: String
+    let sendOptions: String?
+    let session: String
+
+    init(transaction: String, sendOptions: SendOptions?, session: String) throws {
+        self.transaction = transaction
+        self.session = session
+
+        if let sendOptions {
+            let json = try JSONEncoder().encode(sendOptions)
+            self.sendOptions = String(data: json, encoding: .utf8)
+        } else {
+            self.sendOptions = nil
+        }
+    }
+}
+
+struct SignAllTransactionsRequestPayload: Encodable, Sendable {
+    let transactions: [String]
+    let session: String
+}
+
+struct SignTransactionRequestPayload: Encodable, Sendable {
+    let transaction: String
+    let session: String
+}
+
+struct SignMessageRequestPayload: Encodable, Sendable {
+    let message: String
+    let session: String
+    let display: MessageDisplayFormat?
+}
+
+// ***********************************
+// Helper types
+// ***********************************
 
 // SendOptions type based on https://solana-foundation.github.io/solana-web3.js/types/SendOptions.html
 public struct SendOptions: Codable {
@@ -8,7 +55,7 @@ public struct SendOptions: Codable {
     public let skipPreflight: Bool?
 }
 
-public enum MessageDisplayFormat: String {
+public enum MessageDisplayFormat: String, Encodable, Sendable {
     case hex = "hex"
     case utf8 = "utf-8"
 }
