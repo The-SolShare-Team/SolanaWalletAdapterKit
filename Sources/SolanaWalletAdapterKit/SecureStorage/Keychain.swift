ed4aa937d1d226f6a4560fa0f597f92639e17f5e
Samuel Martineau <samumartineau@gmail.com>
2025-11-20 22:30:33 -0500

    implement walletconnectionmanager

--- Sources/SolanaWalletAdapterKit/SecureStorage/Keychain.swift
+++ Sources/SolanaWalletAdapterKit/SecureStorage/Keychain.swift
@@ -2,21 +2,34 @@ import Foundation
 import SimpleKeychain
 
 public struct Keychain: SecureStorage {
-    public init() {}
-    
-    // TODO: Not sure if the service should be hardcoded
-    let keychain = SimpleKeychain(
-        service: "SolanaWalletAdapterKit", accessibility: .whenUnlockedThisDeviceOnly)
+    let keychain: SimpleKeychain
 
-    public func retrieve(key: String) async throws -> Data? {
-        return try? keychain.data(forKey: key)
+    public init(
+        service: String = "SolanaWalletAdapterKit",
+        accessibility: Accessibility = .whenUnlockedThisDeviceOnly
+    ) {
+        self.keychain = SimpleKeychain(service: service, accessibility: accessibility)
+    }
+
+    public init(keychain: SimpleKeychain) {
+        self.keychain = keychain
+    }
+
+    public func retrieve(key: String) async throws -> Data {
+        return try keychain.data(forKey: key)
+    }
+
+    public func retrieveAll() async throws -> [String: Data] {
+        return Dictionary(
+            uniqueKeysWithValues: try keychain.keys().map { ($0, try keychain.data(forKey: $0)) }
+        )
     }
 
     public func store(_ data: Data, key: String) async throws {
-        try? keychain.set(data, forKey: key)
+        try keychain.set(data, forKey: key)
     }
 
     public func clear(key: String) async throws {
-        try? keychain.deleteItem(forKey: key)
+        try keychain.deleteItem(forKey: key)
     }
 }
