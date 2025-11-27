3129f33e9f3e3341c5e896ad4eb1940826a4c836
Dan Chiem <dan@chiem.me>
2025-11-27 14:51:06 -0500

    Refactor getBalance

--- Sources/SolanaRPC/methods/getBalance.swift
+++ Sources/SolanaRPC/methods/getBalance.swift
@@ -1,31 +1,34 @@
 import SolanaTransactions
 
-private struct RequestConfiguration: Encodable {
-    let commitment: Commitment
-    let minContextSlot: Int
-}
-
-
-
 extension SolanaRPCClient {
+    public struct GetBalanceConfiguration: Encodable {
+        let commitment: Commitment?
+        let minContextSlot: Int?
+
+        public init(
+            commitment: Commitment? = nil,
+            minContextSlot: Int? = nil
+        ) {
+            self.commitment = commitment
+            self.minContextSlot = minContextSlot
+        }
+    }
+
     public func getBalance(
-        publicKey: String,
-        configuration: (commitment: Commitment, minContextSlot: Int)? = nil
+        account: PublicKey,
+        configuration: GetBalanceConfiguration? = nil
     )
-    async throws(RPCError) -> UInt64
+        async throws(RPCError) -> UInt64
     {
-        let response = try await fetch(
+        var params: [Encodable] = [account]
+        if let configuration {
+            params.append(configuration)
+        }
+
+        return try await fetch(
             method: "getBalance",
-            params: [
-                publicKey,
-                configuration.map {
-                    RequestConfiguration(
-                        commitment: $0.commitment,
-                        minContextSlot: $0.minContextSlot
-                    )
-                }
-            ],
-            into: RPCResponseResult<UInt64>.self)
-        return response.value
+            params: params,
+            into: RPCResponseResult<UInt64>.self
+        ).value
     }
 }
