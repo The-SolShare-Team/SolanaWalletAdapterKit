b05ae485f308ff37c44f39082787a73a811dfa04
Samuel Martineau <samumartineau@gmail.com>
2025-11-29 18:02:37 -0500

    Fix errors in CryptographicIdentifier initializers

--- Sources/SolanaTransactions/Coding/CryptographicIdentifier.swift
+++ Sources/SolanaTransactions/Coding/CryptographicIdentifier.swift
@@ -15,49 +15,29 @@ public protocol CryptographicIdentifier: ExpressibleByArrayLiteral, ExpressibleB
     static var byteLength: Int { get }
     var bytes: Data { get }
     init?<Bytes: Collection>(bytes: Bytes) where Bytes.Element == UInt8
+    init?(string: String)
 }
 
 protocol _CryptographicIdentifier: SolanaTransactionCodable, CryptographicIdentifier {
     init(bytes: Data)
 }
 
-extension _CryptographicIdentifier {
-    public init?<Bytes: Collection>(bytes: Bytes) where Bytes.Element == UInt8 {
-        if bytes.count != Self.byteLength { return nil }
-        self.init(bytes: Data(bytes))
-    }
-
-    public init(arrayLiteral elements: UInt8...) {
-        precondition(elements.count == Self.byteLength, "Invalid CryptographicIdentifier array literal: \(elements)")
-        self.init(bytes: Data(elements))
-    }
-
-    public init(stringLiteral value: StaticString) {
-        let bytes = Data(base58Encoded: "\(value)")
-        precondition(bytes != nil && bytes!.count == Self.byteLength, "Invalid CryptographicIdentifier string literal: \(value)")
-        self.init(bytes: bytes!)
+extension CryptographicIdentifier {
+    public init?(string: String) {
+        guard let bytes = Data(base58Encoded: string) else { return nil }
+        self.init(bytes: bytes)
     }
 
     public var description: String {
         bytes.base58EncodedString()
     }
 
-    init(fromSolanaTransaction buffer: inout SolanaTransactionBuffer)
-        throws(SolanaTransactionCodingError)
-    {
-        guard let bytes = buffer.readBytes(length: Self.byteLength) else { throw .endOfBuffer }
-        self.init(bytes: Data(bytes))
-    }
-
-    func solanaTransactionEncode(to buffer: inout SolanaTransactionBuffer)
-        throws(SolanaTransactionCodingError)
-    {
-        buffer.writeBytes(bytes)
-    }
-
     public init(fromBorshBuffer buffer: inout BorshByteBuffer) throws(BorshDecodingError) {
         guard let bytes = buffer.readBytes(length: Self.byteLength) else { throw .endOfBuffer }
-        self.init(bytes: Data(bytes))
+        guard let result = Self(bytes: Data(bytes)) else {
+            throw BorshDecodingError.invalidValue
+        }
+        self = result
     }
 
     public func borshEncode(to buffer: inout BorshByteBuffer) throws(BorshEncodingError) {
@@ -76,7 +56,12 @@ extension _CryptographicIdentifier {
             throw DecodingError.dataCorruptedError(
                 in: container, debugDescription: "Invalid Base58 string: \(string)")
         }
-        self.init(bytes: bytes)
+        guard let result = Self(bytes: bytes) else {
+            throw DecodingError.dataCorruptedError(
+                in: container,
+                debugDescription: "Invalid \(Self.self): \(bytes)")
+        }
+        self = result
     }
 
     public func hash(into hasher: inout Hasher) {
@@ -85,5 +70,36 @@ extension _CryptographicIdentifier {
 
     public static func == (lhs: Self, rhs: Self) -> Bool {
         lhs.bytes == rhs.bytes
+    }
+}
+
+extension _CryptographicIdentifier {
+    public init?<Bytes: Collection>(bytes: Bytes) where Bytes.Element == UInt8 {
+        if bytes.count != Self.byteLength { return nil }
+        self.init(bytes: Data(bytes))
+    }
+
+    public init(arrayLiteral elements: UInt8...) {
+        precondition(elements.count == Self.byteLength, "Invalid \(Self.self) array literal: \(elements)")
+        self.init(bytes: Data(elements))
+    }
+
+    public init(stringLiteral value: StaticString) {
+        let bytes = Data(base58Encoded: "\(value)")
+        precondition(bytes != nil && bytes!.count == Self.byteLength, "Invalid \(Self.self) string literal: \(value)")
+        self.init(bytes: bytes!)
+    }
+
+    init(fromSolanaTransaction buffer: inout SolanaTransactionBuffer)
+        throws(SolanaTransactionCodingError)
+    {
+        guard let bytes = buffer.readBytes(length: Self.byteLength) else { throw .endOfBuffer }
+        self.init(bytes: Data(bytes))
+    }
+
+    func solanaTransactionEncode(to buffer: inout SolanaTransactionBuffer)
+        throws(SolanaTransactionCodingError)
+    {
+        buffer.writeBytes(bytes)
     }
 }
