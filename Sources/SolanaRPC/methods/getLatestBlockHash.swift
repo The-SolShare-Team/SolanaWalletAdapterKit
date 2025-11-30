c5cb282ea43a612590026b9e873cf9867ef2724b
Nuthanan Tharmarajah <n2tharma@uwaterloo.ca>
2025-11-30 00:10:33 -0500

    SolanaRPC documentation complete

--- Sources/SolanaRPC/methods/getLatestBlockHash.swift
+++ Sources/SolanaRPC/methods/getLatestBlockHash.swift
@@ -1,6 +1,25 @@
 import SolanaTransactions
 
 extension SolanaRPCClient {
+    /// Configuration options for the ``getLatestBlockhash(configuration:)`` RPC request.
+    ///
+    /// This struct allows you to provide **optional parameters** when requesting
+    /// the latest blockhash from the Solana blockchain. All properties are optional,
+    /// so you can specify only the values you need.
+    ///
+    ///```
+    ///public init(
+    ///     commitment: Commitment? = nil
+    ///     minContextSlot: Int? = nil
+    ///) {
+    ///     self.commitment = commitment
+    ///     self.minContextSlot = minContextSlot
+    /// }
+    /// ```
+    ///
+    /// - Parameters:
+    ///   - commitment: The commitment describes how finalized a block is at that point in time. See ``Commitment``.
+    ///   - minContextSlot: The minimum slot that the request can be evaluated at.`.
     public struct GetLatestBlockhashConfiguration: Encodable {
         let commitment: Commitment?
         let minContextSlot: Int?
@@ -14,12 +33,33 @@ extension SolanaRPCClient {
         }
     }
 
+    /// Response returned from the `getLatestBlockhash` RPC request to the Solana network.
+    ///
+    /// Use this struct to access the details of the latest blockhash when querying the network.
+    ///
+    /// - Properties:
+    ///   - blockhash: The most recent blockhash on the network. This is typically used
+    ///                when constructing transactions to ensure they are valid.
+    ///   - lastValidBlockHeight: The last block height at which the returned blockhash
+    ///                           remains valid. Transactions referencing this blockhash
+    ///                           must be submitted before this height to be accepted.
     public struct GetLatestBlockhashResponse: Decodable {
         public let blockhash: Blockhash
         public let lastValidBlockHeight: UInt64
     }
 
-    /// https://solana.com/docs/rpc/http/getlatestblockhash
+    /// Returns the latest blockhash
+    ///
+    /// This method sends a `getLatestBlockhash` RPC request to the Solana network. You can optionally provide a
+    /// ``GetLatestBlockhashConfiguration`` to control things like the commitment level or the minimum context slot.
+    ///
+    /// - Parameters:
+    ///   - configuration: Optional configuration for the request, such as
+    ///                    commitment level and minimum context slot. Defaults to `nil`. See ``GetLatestBlockhashConfiguration``
+    ///
+    /// - Returns: The blockhash and lastValidBlockHeight in the struct, ``GetLatestBlockhashResponse``
+    ///
+    /// - Throws: `RPCError` if the request fails or the response is invalid.
     public func getLatestBlockhash(
         configuration: GetLatestBlockhashConfiguration? = nil
     ) async throws(RPCError) -> GetLatestBlockhashResponse {
--- Sources/SolanaRPC/methods/sendTransaction.swift
+++ Sources/SolanaRPC/methods/sendTransaction.swift
@@ -3,6 +3,27 @@ import SolanaTransactions
 import SwiftBorsh
 
 extension SolanaRPCClient {
+    /// Configuration options for the ``sendTransaction(transaction:configuration:)``  RPC request.
+    ///  
+    /// This struct allows you to provide **optional parameters** sending a transaction on the Solana blockchain. All properties are optional,
+    /// so you can specify only the values you need.
+    ///
+    /// ```
+    /// public init(
+    ///     encoding: TransactionEncoding? = nil,
+    ///     skipPreflight: Bool? = nil,
+    ///     preflightCommitment: Commitment? = nil,
+    ///     maxRetries: Int? = nil,
+    ///     minContextSlot: Int? = nil
+    /// ) {
+    /// ```
+    ///
+    /// - Parameters:
+    ///   - encoding: Encoding used for the transaction data. Values: `.base58` (slow, DEPRECATED), or `.base64`.
+    ///   - skipPreflight: When true, skip the preflight transaction checks.
+    ///   - preflightCommitment: Commitment level to use for preflight. See ``Commitment``
+    ///   - maxRetries: Maximum number of times for the RPC node to retry sending the transaction to the leader. If this parameter not provided, the RPC node will retry the transaction until it is finalized or until the blockhash expires.
+    ///   - minContextSlot: Set the minimum slot at which to perform preflight transaction checks
     public struct SendTransactionConfiguration: Encodable {
         let encoding: TransactionEncoding?
         let skipPreflight: Bool?
@@ -30,7 +51,27 @@ extension SolanaRPCClient {
         case base64
     }
 
-    /// https://solana.com/docs/rpc/http/sendtransaction
+    /// Submits a signed transaction to the cluster for processing.
+    ///
+    /// This method does not alter the transaction in any way; it relays the transaction created by clients to the node as-is.
+    ///
+    /// If the node's rpc service receives the transaction, this method immediately succeeds, without waiting for any confirmations. A successful response from this method does not guarantee the transaction is processed or confirmed by the cluster.
+    ///
+    /// While the rpc service will reasonably retry to submit it, the transaction could be rejected if transaction's` recent_blockhash` expires before it lands.
+    ///
+    /// Before submitting, the following preflight checks are performed:
+    /// 1. The transaction signatures are verified
+    /// 2. The transaction is simulated against the bank slot specified by the preflight commitment. On failure an error will be returned. Preflight checks may be disabled if desired. It is recommended to specify the same commitment and preflight commitment to avoid confusing behavior.
+    /// The returned signature is the first signature in the transaction, which is used to identify the transaction (transaction id). This identifier can be easily extracted from the transaction data before submission.
+    ///
+    /// See [sendTransaction](https://solana.com/docs/rpc/http/sendtransaction) on Solana documentation for more details.
+    /// - Parameters:
+    ///   - transaction: Fully-signed Transaction, as encoded string. See ``TransactionEncoding``
+    ///   - configuration: Optional configuration for the request. Defaults to `nil`. See ``SendTransactionConfiguration``
+    ///
+    /// - Returns: First Transaction Signature embedded in the transaction, as base-58 encoded string.
+    ///
+    /// - Throws: `RPCError` if the request fails or the response is invalid.
     public func sendTransaction(
         transaction: Transaction,
         configuration: SendTransactionConfiguration? = nil
